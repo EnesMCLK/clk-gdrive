3dd0928dd49021d4f35e389a3edd451cc1528ca2
Enes ÇELİK <40140673+EnesMCLK@users.noreply.github.com>
2026-03-02 21:02:39 +0300

    Create build.js

---
+++ build.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+
+try {
+    // 1. index.html dosyasını oku
+    let content = fs.readFileSync('index.html', 'utf8');
+    
+    // 2. Ortam değişkenini al. Alamazsa boş bırakır.
+    const scriptUrl = process.env.APPS_SCRIPT_URL || '';
+    
+    if(!scriptUrl) {
+        console.warn("UYARI: APPS_SCRIPT_URL ortam değişkeni bulunamadı!");
+    }
+
+    // 3. Değişkeni HTML içine yerleştir
+    content = content.replace('__APPS_SCRIPT_URL__', scriptUrl);
+    
+    // 4. Dosyayı yeni haliyle kaydet
+    fs.writeFileSync('index.html', content);
+    
+} catch (err) {
+    console.error("HATA: Dosya değiştirilemedi!", err);
+    process.exit(1); // Hata verirse derlemeyi durdur
+}
